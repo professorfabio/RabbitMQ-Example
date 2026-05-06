927e0e8175af1cc183c89cf41e237a5d89dc0830
Fábio M. Costa <fmc@ufg.br>
2026-05-06 15:40:37 -0300

    Implement RabbitMQ producer in producer.py

---
+++ producer.py
@@ -0,0 +1,14 @@
+import rabbitpy
+from const import *
+
+with rabbitpy.Connection('amqp://myuser:abc123@'+ RABBITMQ_ADDR + ':5672/%2f') as conn:
+    with conn.channel() as channel:
+        exchange = rabbitpy.Exchange(channel, 'my-exchange')
+        exchange.declare()
+
+        queue = rabbitpy.Queue(channel, 'my-queue')
+        queue.declare()
+        queue.bind(exchange, 'my-routing-key')
+
+        message = rabbitpy.Message(channel, 'Hello, world!')
+        message.publish(exchange, 'my-routing-key')
