e4dcd4e2aec3b296a6dbbdddd4c62f6c44b01f58
Fábio M. Costa <fmc@ufg.br>
2026-05-06 15:34:35 -0300

    Change RabbitMQ connection credentials
    Updated RabbitMQ connection string to use new credentials.

--- consumer.py
+++ consumer.py
@@ -2,7 +2,6 @@ import rabbitpy
 from const import *
 
 def consumer():
-  #connection = rabbitpy.Connection('amqp://guest:guest@' + RABBITMQ_ADDR + ':5672/%2f')
   connection = rabbitpy.Connection('amqp://fmc:abc123@' + RABBITMQ_ADDR + ':5672/%2f')
   channel = connection.channel()
 
