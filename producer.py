6bb11afe6236ca7edeef34e75c690a0c80c40844
Fábio M. Costa <fmc@ufg.br>
2026-05-06 16:14:27 -0300

    Fix queue declaration parameters in producer.py

--- producer.py
+++ producer.py
@@ -6,8 +6,8 @@ with rabbitpy.Connection('amqp://myuser:abc123@'+ RABBITMQ_ADDR + ':5672/my_vhos
         exchange = rabbitpy.Exchange(channel, 'my-exchange')
         exchange.declare()
 
-        queue = rabbitpy.Queue(channel, 'my-queue', exclusive=True)
-        queue.declare(auto_delete=False)
+        queue = rabbitpy.Queue(channel, 'my-queue', exclusive=True, auto_delete=False)
+        queue.declare()
         queue.bind(exchange, 'my-routing-key')
 
         message = rabbitpy.Message(channel, 'Hello, world!')
