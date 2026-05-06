86a22953b7dd31cc7080a434ac0a2c517b2c48b9
Fábio M. Costa <fmc@ufg.br>
2026-05-06 16:05:16 -0300

    Refactor queue handling in consumer.py

--- consumer.py
+++ consumer.py
@@ -3,6 +3,8 @@ from const import *
 
 with rabbitpy.Connection('amqp://myuser:abc123@' + RABBITMQ_ADDR + ':5672/%2f') as conn:
     with conn.channel() as channel:
-        for message in rabbitpy.Queue(channel, 'my-queue', exclusive=True):
+        queue = rabbitpy.Queue(channel, 'my-queue', exclusive=True)
+        queue.declare()
+        for message in queue:
             print(message.body)
             message.ack()
