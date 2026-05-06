083e4294299925973acdfc383e5121b7321f1ff1
Fábio M. Costa <fmc@ufg.br>
2026-05-06 16:11:50 -0300

    Update consumer.py

--- consumer.py
+++ consumer.py
@@ -1,10 +1,9 @@
 import rabbitpy
 from const import *
 
-with rabbitpy.Connection('amqp://myuser:abc123@' + RABBITMQ_ADDR + ':5672/%2f') as conn:
+with rabbitpy.Connection('amqp://myuser:abc123@' + RABBITMQ_ADDR + ':5672/my_vhost') as conn:
     with conn.channel() as channel:
         queue = rabbitpy.Queue(channel, 'my-queue', exclusive=True)
-        queue.declare()
         for message in queue:
             print(message.body)
             message.ack()
