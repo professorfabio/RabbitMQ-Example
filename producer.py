3a0db089638969c5ecaa5561cd73436bd32af6be
Fábio M. Costa <fmc@ufg.br>
2026-05-06 15:51:27 -0300

    Refactor queue declaration to use constructor argument

--- producer.py
+++ producer.py
@@ -6,8 +6,8 @@ with rabbitpy.Connection('amqp://myuser:abc123@'+ RABBITMQ_ADDR + ':5672/%2f') a
         exchange = rabbitpy.Exchange(channel, 'my-exchange')
         exchange.declare()
 
-        queue = rabbitpy.Queue(channel, 'my-queue')
-        queue.declare(exclusive=True)
+        queue = rabbitpy.Queue(channel, 'my-queue', exclusive=True)
+        queue.declare()
         queue.bind(exchange, 'my-routing-key')
 
         message = rabbitpy.Message(channel, 'Hello, world!')
