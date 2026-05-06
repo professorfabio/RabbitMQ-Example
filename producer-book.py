2e1c50c7b7af908f7cee2e08474f965702267a67
Fábio M. Costa <fmc@ufg.br>
2026-05-06 16:19:55 -0300

    Update producer-book.py

--- producer-book.py
+++ producer-book.py
@@ -2,16 +2,16 @@ import rabbitpy
 from const import *
 
 def producer():
-  connection = rabbitpy.Connection('amqp://fmc:abc123@' + RABBITMQ_ADDR + ':5672/%2f') # Connect to RabbitMQ server
+  connection = rabbitpy.Connection('amqp://myuser:abc123@' + RABBITMQ_ADDR + ':5672/%2f') # Connect to RabbitMQ server
   channel = connection.channel()     # Create new channel on the connection
 
   exchange = rabbitpy.Exchange(channel, 'exchange') # Create an exchange
   exchange.declare()
 
-  queue1 = rabbitpy.Queue(channel, 'example1') # Create 1st queue
+  queue1 = rabbitpy.Queue(channel, 'example1', durable=True, auto_delete=False) # Create 1st queue
   queue1.declare()
 
-  queue2 = rabbitpy.Queue(channel, 'example2') # Create 2nd queue
+  queue2 = rabbitpy.Queue(channel, 'example2', durable=True, auto_delete=False) # Create 2nd queue
   queue2.declare()
 
   queue1.bind(exchange, 'example-key') # Bind queue1 to a single key
