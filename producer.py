660965b45c473e8e448ea675e5ee49a43d5e1498
Fábio M. Costa <fmc@ufg.br>
2026-05-06 16:17:06 -0300

    Change queue to be durable in producer.py

--- producer.py
+++ producer.py
@@ -6,7 +6,7 @@ with rabbitpy.Connection('amqp://myuser:abc123@'+ RABBITMQ_ADDR + ':5672/my_vhos
         exchange = rabbitpy.Exchange(channel, 'my-exchange')
         exchange.declare()
 
-        queue = rabbitpy.Queue(channel, 'my-queue', exclusive=True, auto_delete=False)
+        queue = rabbitpy.Queue(channel, 'my-queue', durable=True, auto_delete=False)
         queue.declare()
         queue.bind(exchange, 'my-routing-key')
 
