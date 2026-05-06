7cafd64556645defbadb352ce0a645b0fd496bb5
Fábio M. Costa <fmc@ufg.br>
2026-05-06 15:58:47 -0300

    Fix typo in Queue instantiation with exclusive flag

--- consumer.py
+++ consumer.py
@@ -3,6 +3,6 @@ from const import *
 
 with rabbitpy.Connection('amqp://myuser:abc123@' + RABBITMQ_ADDR + ':5672/%2f') as conn:
     with conn.channel() as channel:
-        for message in rabbitpy.Queue(channel, 'my-queue'):
+        for message in rabbitpy.Queue(channel, 'my-queue', eclusive=True):
             print(message.body)
             message.ack()
